da01397da2a24cedad7c04cd7d4d9d8bce10228d
pptp78ec <pptp78ec@gmail.com>
2023-09-13 20:08:42 +0300

    Notifications DB + controller

---
+++ TilePlanner_Server_RESTAPI/TilePlanner_Server_RESTAPI/ORM/Notification.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace TilePlanner_Server_RESTAPI.ORM
+{
+    public class Notification
+    {
+        [BsonId]
+        public string Id { get; set; } = string.Empty;
+        public DateTime NotificationTime { get; set; } = DateTime.MinValue;
+        public string Header { get; set; } = string.Empty;
+        public bool IsDone { get; set; } = false;
+        public string UserId { get; set; } = string.Empty;
+    }
+}
